ba5e5ef6e7fa81ccb13de4684a110fb627c192b0
Taylor <taylorlange95@gmail.com>
2023-11-01 15:03:56 -0500

    completed longest word exercise

--- algorithms/1-Strings/longestWord.js
+++ algorithms/1-Strings/longestWord.js
@@ -7,16 +7,61 @@ Read through the function and write comments to explain what is happening in the
  2. How would you display the actual word in the console?
  */
 
-function findLongestWord(str) {
-  var words = str.split(' ');
-  var longestWordLength = 0;
+// function findLongestWord(str) {
+//   var words = str.split(' ');
+//   var longestWordLength = 0;
+  
+//   for (var i = 0; i < words.length; i++) {
+//     if (words[i].length > longestWordLength) {
+//       longestWordLength = words[i].length;
+//     }
+//   }
+  
+//   return longestWordLength;
+// }
+
+//1. We can alter the existing function or create a new function that finds the shortest word by slightly modifying the above if statement:
+
+// function findShortestWord(str) {
+//   let words = str.split(' ');
+//   let shortestWordLength = Infinity; 
+  
+//   for (var i = 0; i < words.length; i++) {
+//     if (words[i].length < shortestWordLength) {
+//       shortestWordLength = words[i].length;
+//     }
+//   }
+
+//   if (shortestWordLength === Infinity) {
+//     return 0; 
+//   }
+  
+//   return shortestWordLength;
+//}
+
+// By setting the value to Infinity, it simply ensures that the initial value is larger than any potential input in the string.
+
+//2. To actually display the shortest (or longest word, depending on what function we want to use) we  can create a variable that will hold the word itself and then update the function's loop to keep track of what the shortest word is and then actually return it or display it in the console: 
+
+function findShortestWord(str) {
+  let words = str.split(' ');
+  let shortestWordLength = Infinity; 
+  let shortestWord = "";
   
   for (var i = 0; i < words.length; i++) {
-    if (words[i].length > longestWordLength) {
-      longestWordLength = words[i].length;
+    if (words[i].length < shortestWordLength) {
+      shortestWordLength = words[i].length;
+      shortestWord = words[i];
     }
   }
+
+  if (shortestWordLength === Infinity) {
+    return { length: 0, word: '' };
+  }
   
-  return longestWordLength;
+  return { length: shortestWordLength, word: shortestWord };
 }
 
+let result = findShortestWord("This is a test sentence");
+console.log("Shortest Word:", result.word);
+console.log("Shortest Word Length:", result.length);
