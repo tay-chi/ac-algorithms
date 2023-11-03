5f7b7354c290ef063c54ae9fbc30f67cd7454373
Taylor <taylorlange95@gmail.com>
2023-11-03 14:47:07 -0500

    completed decision tree exercise

--- algorithms/Decision-tree-graphic/decision-tree.js
+++ algorithms/Decision-tree-graphic/decision-tree.js
@@ -1,3 +1,28 @@
+let weather = prompt("What's the weather like? (Sun, Clouds, Rain)");
+let time = (" ");
+let hungry = false;
+
+
 function transportationOptions() {
-  
+
+  if (weather === "Sun") {
+    time = prompt("What time do you have to get there?");
+    if (time >= 30) {
+      console.log("You should walk.");
+    } else if (time < 30) {
+      console.log("You should take the bus.");
+    }
+  } else if (weather === "Clouds") {
+    hungry = prompt("Are you hungry? (Yes or No)").toLowerCase();
+    if (hungry === "yes") {
+      console.log("You should walk.");
+    } else {
+      console.log("You should take the bus.");
+    }
+  } else {
+    console.log("You should take the bus.");
+  }
 }
+
+transportationOptions();
+
